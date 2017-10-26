28966d832160208e357c89e5fce8f11f931ab539
zofia.owsiak <owsiak@mops.uci.agh.edu.pl>
2017-10-26 13:55:14 +0200

    Nowe matrixy

---
+++ Matrix/test/MatrixTest.java
@@ -0,0 +1,72 @@
+import static org.junit.Assert.*;
+
+public class MatrixTest {
+    @org.junit.Test
+    public void asArray() throws Exception {
+    }
+
+    @org.junit.Test
+    public void get() throws Exception {
+    }
+
+    @org.junit.Test
+    public void set() throws Exception {
+    }
+
+    @org.junit.Test
+    public void toString() throws Exception {
+    }
+
+    @org.junit.Test
+    public void reshape() throws Exception {
+    }
+
+    @org.junit.Test
+    public void add() throws Exception {
+    }
+
+    @org.junit.Test
+    public void div() throws Exception {
+    }
+
+    @org.junit.Test
+    public void sub() throws Exception {
+    }
+
+    @org.junit.Test
+    public void mul() throws Exception {
+    }
+
+    @org.junit.Test
+    public void add1() throws Exception {
+    }
+
+    @org.junit.Test
+    public void sub1() throws Exception {
+    }
+
+    @org.junit.Test
+    public void mul1() throws Exception {
+    }
+
+    @org.junit.Test
+    public void div1() throws Exception {
+    }
+
+    @org.junit.Test
+    public void shape() throws Exception {
+    }
+
+    @org.junit.Test
+    public void dot() throws Exception {
+    }
+
+    @org.junit.Test
+    public void forbenius() throws Exception {
+    }
+
+    @org.junit.Test
+    public void random() throws Exception {
+    }
+
+}
